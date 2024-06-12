59011cad35f4924a679f96296726a8f4081681c1
a1eksek <alexey20080705@gmail.com>
2024-06-12 20:36:26 +0400

    add file 2

--- main.py
+++ main.py
@@ -1,16 +1,76 @@
-# This is a sample Python script.
+# def func():
+#     pass
+#
+# class Test:
+#     pass
+#
+# print(type(5))
+# print(type('a'))
+# print(type(5.5))
+# print(type(True))
+# print(type([1, 2]))
+# print(type((5, 4)))
+# print(type({1, 2}))
+# print(type({'a': 2}))
+# print(type(Test))
+# print(type(func))
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# class Person:
+#     name = "Лёша"
+#     age = 42
+#
+#     def say(self):
+#         print("hello")
+#
+# Alexey = Person()
+# print(Alexey.name)
+# print(Alexey.age)
+# Alexey.say()
+#
+# Alexey2 = Person()
+# Alexey2.name = "Не Лёша"
+# print(Alexey2.name)
+#
+# class person:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+#
+# person1 = person('Иван', 15)
+# person2 = person('Пётр', 14)
+#
+# print(person1.name)
+# print(person1.age)
+# print(person2.name)
+# print(person2.age)
+#
+# class person:
+#     def __init__(self, name, lastname):
+#         self.name = name
+#         self.lastname = lastname
+#
+#     def __str__(self):
+#         return f'Меня зовут {self.name} {self.lastname}'
+#
+# person1 = person('Иван', 'Иванов')
+# print(person1)
 
+class person:
+    def __init__(self, firstname, lastname, status, numder):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.status = status
+        self.numder = numder
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    def __str__(self):
+        return f'Меня зовут {self.firstname} {self.lastname}, я учусь на {self.status} и я нахожусь на {self.numder} месте в списке'
 
+person1 = person("Иван", "Иванов", "5", "23")
+person2 = person("Алексей", "Рыбаков", "4", "25")
+person3 = person("Пётр", "Соколов", "3", "21")
+person4 = person("Мария", "Воровна", "5", "30")
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print(person1)
+print(person2)
+print(person3)
+print(person4)
